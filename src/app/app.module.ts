468e65c64c682eab4046a499bb944d6e29c68316
Lea369 <coston.lea@gmail.com>
2020-03-27 12:05:37 +0100

    #1 plus structure generale

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContentComponent } from './content/content.component';
+import { AllEtudiantComponent } from './content/etudiant/all-etudiant/all-etudiant.component';
+import { CreateEtudiantComponent } from './content/etudiant/create-etudiant/create-etudiant.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    FooterComponent,
+    ContentComponent,
+    AllEtudiantComponent,
+    CreateEtudiantComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/content/etudiant/all-etudiant/all-etudiant.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-all-etudiant',
+  templateUrl: './all-etudiant.component.html',
+  styleUrls: ['./all-etudiant.component.css']
+})
+export class AllEtudiantComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
