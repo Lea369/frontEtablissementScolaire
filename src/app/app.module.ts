c3ec54f073afd87d8edc81eca7aab04b41353dd6
CloarecTM <tm.cloarec@gmail.com>
2020-03-27 16:20:19 +0100

    #2 Dossier Note

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,9 @@ import { AllEtudiantComponent } from './content/etudiant/all-etudiant/all-etudia
 import { CreateEtudiantComponent } from './content/etudiant/create-etudiant/create-etudiant.component';
 import { AppRoutingModule } from './app-routing.module';
 import { DashboardComponent } from './content/dashboard/dashboard.component';
+import { AllNoteComponent } from './content/note/all-note/all-note.component';
+import { CreateNoteComponent } from './content/note/create-note/create-note.component';
+import { UpdateNoteComponent } from './content/note/update-note/update-note.component';
 
 @NgModule({
   declarations: [
@@ -20,7 +23,10 @@ import { DashboardComponent } from './content/dashboard/dashboard.component';
     NavBarComponent,
     AllEtudiantComponent,
     CreateEtudiantComponent,
-    DashboardComponent
+    DashboardComponent,
+    AllNoteComponent,
+    CreateNoteComponent,
+    UpdateNoteComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/content/note/all-note/all-note.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { EtudiantUpdateDto } from 'src/app/models/etudiant-update-dto';
+import { NoteUpdateDto } from 'src/app/models/note-update-dto';
+import { NoteService } from 'src/app/services/note/note.service';
+
+@Component({
+  selector: 'app-all-note',
+  templateUrl: './all-note.component.html',
+  styleUrls: ['./all-note.component.css']
+})
+export class AllNoteComponent implements OnInit {
+  allNote = new Array<NoteUpdateDto>();
+
+  constructor(private service: NoteService) { }
+
+  ngOnInit() {
+    this.getAll();
+  }
+
+  getAll() {
+    this.service.getAll().subscribe(
+      (responseDto) => {
+        console.log('debug responseDto : ', responseDto);
+        if (!responseDto.error) {
+          this.allNote = responseDto.body;
+        }
+      }
+    );
+  }
+
+  delete(id: number) {
+    this.service.delete(id).subscribe(
+      responseDto => {
+        console.log('debug responseDto : ', responseDto);
+        if (!responseDto.error) {
+          this.allNote = this.allNote.filter(
+            element =>  element.id !== id
+          );
+        }
+        console.log('result after delete: ', this.allNote);
+      }
+    );
+  }
+
+}
