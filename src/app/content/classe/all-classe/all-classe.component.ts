ca0fd643fcaa10e06b588be66ad1805f582b0e9f
jmmathevet <jmmathevet@gmail.com>
2020-03-27 20:38:13 +0100

    #4 modification du all-classe component

--- src/app/content/classe/all-classe/all-classe.component.ts
+++ src/app/content/classe/all-classe/all-classe.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ClasseUpdateDto } from 'src/app/models/classe-update-dto';
+import { ClassesService } from 'src/app/services/classe/classes.service';
+import { ClasseCreateDto } from 'src/app/models/classe-create-dto';
 
 @Component({
   selector: 'app-all-classe',
@@ -7,9 +10,60 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AllClasseComponent implements OnInit {
 
-  constructor() { }
+  allClasse = new Array<ClasseUpdateDto>();
+  messageValidation: string;
+  messageEchec: string;
+  classe = new ClasseCreateDto();
+  
+  constructor(private service: ClassesService) {}
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.getAll();
   }
+
+  getAll() {
+    this.service.getAll().subscribe(
+      (responseDto) => {
+        console.log('debug responseDto : ', responseDto);
+        if (!responseDto.error) {
+          this.allClasse = responseDto.body;
+        }
+        
+      }
+    );
+  }
+
+  delete(id: number) {
+    this.service.delete(id).subscribe(
+      responseDto => {
+        console.log('debug responseDto : ', responseDto);
+        if (!responseDto.error) {
+          this.allClasse = this.allClasse.filter(
+            element =>  element.id !== id
+          );
+        }
+        console.log('result after delete: ', this.allClasse);
+      }
+    );
+  }
+
+  save() {
+    this.service.create(this.classe).subscribe(
+      //SUCCESS ==> 200
+      (responseDto) => {
+        if (!responseDto.error) {
+          this.messageValidation = 'Creation reussie';
+          document.location.reload();
+        }
+      },
+      //FAIL ==> 400, 404, 500...
+      (responseDtoError) => {
+        if (responseDtoError.error) {
+          this.messageEchec = 'Erreur de création';
+        }
+      }
+    );
+  }
+
 
 }
