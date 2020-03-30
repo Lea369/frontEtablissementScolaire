10cd60816284f35eee402a4ab3ded15157a423e8
jmmathevet <jmmathevet@gmail.com>
2020-03-30 14:21:25 +0200

    Ajout du module FormReactiveModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ContentComponent } from './content/content.component';
@@ -63,7 +64,8 @@ import { NoteEtudiantComponent } from './content/etudiant/note-etudiant/note-etu
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [
     HttpClientModule
